4b320039678446fafd766deb7216298c432b1ff6
su9257 <su9257@outlook.com>
2019-05-21 23:25:27 +0800

    示例添加6/7

---
+++ Assets/OptimizeExample/7.AnimationColor/ChangeMaterialColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeMaterialColor : MonoBehaviour
+{
+    public Color color = Color.white;
+    public Image image = null;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.material = Instantiate(image.material) as Material;
+    }
+
+
+    void LateUpdate()
+    {
+        image.material.SetColor("_Color", color);
+    }
+}
