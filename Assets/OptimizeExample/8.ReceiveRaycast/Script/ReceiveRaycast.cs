15b688a9d7de5dc2a55effefdf5e562c36059a4e
su9257 <su9257@outlook.com>
2019-05-22 23:04:54 +0800

    增加示例8/9/10

---
+++ Assets/OptimizeExample/8.ReceiveRaycast/Script/ReceiveRaycast.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiveRaycast : MonoBehaviour
+{
+   
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => { Debug.Log("菜鸟海澜"); });
+    }
+
+}
